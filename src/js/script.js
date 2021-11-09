7555b799c3f01da812bf27f4eb29cfa82cb8df12
Marcus Toma <best4tdm@gmail.com>
2021-11-09 08:26:47 +0100

    moved search script

--- src/js/script.js
+++ src/js/script.js
@@ -30,4 +30,14 @@ function rechercher(contenu_requete, trigger_resultat) {
     xmlhttp.send();
 }
 
-
+function searchButtonClicked(idBarrePlats, idBarrePays) {
+    searchContent = document.getElementById(idBarrePlats).value;
+    countryContent = document.getElementById(idBarrePays).value;
+    if (searchContent != '' || countryContent != '') {
+        // launch search !
+        // redirect with get parameters (if defined) of both !
+        // the other page, on load, gets parameters, fetches content based on it and renders
+        redirectParameters = '?' + (searchContent != ''? 'search='+searchContent : '') + (searchContent != '' && countryContent != ''? '&' : '') + (countryContent != ''? 'country='+countryContent : '');
+        window.location.href = window.location.href.replace('/index.html', '/recherche.html' + redirectParameters);
+    }
+}
